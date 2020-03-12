8af13d8dc446bbf97bbf1df5787732ddabd7c432
sainath <g.sainath2312@gmail.com>
2020-03-12 16:31:14 +0530

    Initial Commit

---
+++ FantasyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FantasyTest
+//
+//  Created by Sainath Gajavada on 12/03/20.
+//  Copyright © 2020 Sainath Gajavada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
